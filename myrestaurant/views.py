e4d134030f73db56b566a4de83444688c2cab846
Oleksandr <kozlov2777@gmail.com>
2023-03-05 13:14:47 +0200

    create new order, details

--- myrestaurant/views.py
+++ myrestaurant/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
-from django.http import HttpResponse
 from django.db import connection, transaction
 from django.shortcuts import render
+from django.utils import timezone
+from .models import MenuItem, Order, OrderItem
+from django.http import HttpResponseServerError
 
 
 def menu_view(request):
@@ -40,7 +41,7 @@ def category_view(request):
 
 def orders_view(request):
     with connection.cursor() as cursor:
-        cursor.execute('SELECT o.id, o.table_number, o.created_at, SUM(oi.quantity * m.price) AS total_price FROM myrestaurant_order o JOIN myrestaurant_orderitem oi ON o.id = oi.order_id JOIN myrestaurant_menuitem m ON oi.item_id = m.id GROUP BY o.id')
+        cursor.execute('SELECT o.id, o.table_number, o.created_at, SUM(oi.quantity * m.price) AS total_price FROM myrestaurant_order o JOIN myrestaurant_orderitem oi ON o.id = oi.order_id JOIN myrestaurant_menuitem m ON oi.item_id = m.id GROUP BY o.id ORDER BY o.created_at DESC')
 
         rows = cursor.fetchall()
         order_list = []
@@ -54,28 +55,81 @@ def orders_view(request):
     return render(request, 'order_list.html', {'order_list': order_list})
 
 
-def add_order_view(request):
+def new_order(request):
     if request.method == 'POST':
         table_number = request.POST['table_number']
-        created_at = request.POST['created_at']
-        items = request.POST.getlist('item[]')
-        quantities = request.POST.getlist('quantity[]')
-        with transaction.atomic():
-            with connection.cursor() as cursor:
-                cursor.execute('INSERT INTO myrestaurant_order (table_number, created_at) VALUES (%s, %s)', [table_number, created_at])
-                order_id = cursor.lastrowid
-                for i in range(len(items)):
-                    cursor.execute('INSERT INTO myrestaurant_orderitem (order_id, item_id, quantity) VALUES (%s, %s, %s)', [order_id, items[i], quantities[i]])
-        return HttpResponse('Order added successfully')
-    else:
+        items = request.POST.getlist('item')
+        quantities = request.POST.getlist('quantity')
+
+        # Встановлюємо з'єднання з базою даних та отримуємо курсор
         with connection.cursor() as cursor:
-            cursor.execute('SELECT id, name FROM menu')
+            # Вставляємо новий запис в таблицю Order та отримуємо його id
+            cursor.execute("INSERT INTO myrestaurant_order (table_number, created_at) VALUES (%s, %s)", [table_number, timezone.now()])
+            order_id = cursor.lastrowid
+
+            # Додаємо записи у таблицю OrderItem для кожного замовленого елементу
+            for i in range(len(items)):
+                item_id = items[i]
+                quantity = quantities[i]
+                cursor.execute("INSERT INTO myrestaurant_orderitem (order_id, item_id, quantity) VALUES (%s, %s, %s)", [order_id, item_id, quantity])
+
+        # Отримуємо створене замовлення з бази даних
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM myrestaurant_order WHERE id = %s", [order_id])
+            order_data = cursor.fetchone()
+
+            cursor.execute("SELECT myrestaurant_menuitem.*, myrestaurant_orderitem.quantity FROM myrestaurant_menuitem JOIN myrestaurant_orderitem ON myrestaurant_menuitem.id = myrestaurant_orderitem.item_id WHERE myrestaurant_orderitem.order_id = %s", [order_id])
+            items_data = cursor.fetchall()
+
+        # Створюємо об'єкти моделей для створеного замовлення
+        order = Order(id=order_data[0], table_number=order_data[1], created_at=order_data[2])
+        order_items = [OrderItem(item=MenuItem(id=item[0], name=item[1], price=item[2]), quantity=item[3]) for item in items_data]
+
+        return render(request, 'order_detail.html', {'order': order, 'order_items': order_items})
+    else:
+        menu_items = MenuItem.objects.all()
+        return render(request, 'new_order.html', {'menu_items': menu_items})
+
+
+def order_detail(request, order_id):
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute(
+                "SELECT oi.id, oi.quantity, m.name, m.price FROM myrestaurant_orderitem oi JOIN myrestaurant_menuitem m ON oi.item_id = m.id WHERE oi.order_id =%s", [order_id]
+            )
             rows = cursor.fetchall()
-            menu_items = []
-            for row in rows:
-                menu_items.append({
-                    'id': row[0],
-                    'name': row[1],
-                })
-        context = {'menu_items': menu_items}
-        return HttpResponse(context)
+            if rows is None:
+                return HttpResponseServerError('Order item does not exist.')
+            else:
+                detail_list = []
+                for row in rows:
+                    detail_list.append({
+                        'id': row[0],
+                        'quantity': row[1],
+                        'item_name': row[2],
+                        'item_price': row[3],
+                    })
+                return render(request, 'order_detail.html', {'detail_list': detail_list})
+    except:
+        return HttpResponseServerError('Error retrieving order item.')
+
+
+def order_detail_by_table_number(request, table_number):
+    with connection.cursor() as cursor:
+        cursor.execute(
+            "SELECT o.id, m.name, oi.quantity, o.created_at, SUM(m.price * oi.quantity) as total FROM myrestaurant_order o INNER JOIN myrestaurant_orderitem oi ON o.id = oi.order_id INNER JOIN myrestaurant_menuitem m ON oi.item_id = m.id WHERE o.table_number = %s GROUP BY o.id, m.id ORDER BY o.created_at DESC;",
+            [table_number]
+        )
+        rows = cursor.fetchall()
+        detail_by_table = []
+        for row in rows:
+            detail_by_table.append({
+                'id': row[0],
+                'name': row[1],
+                'quantity': row[2],
+                'created_at': row[3],
+                'total': row[4],
+            })
+        return render(request, 'order_detail_by_table_number.html', {'detail_by_table': detail_by_table})
+
+
--- restaurant/urls.py
+++ restaurant/urls.py
@@ -4,8 +4,10 @@ from myrestaurant import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('add_order/', views.add_order_view, name='add_order'),
-    path('view_order_list/', views.orders_view, name='view_order_list'),
+    path('', views.orders_view, name='view_order_list'),
     path('view_menu/', views.menu_view, name='view_menu_list'),
-    path('category/', views.category_view, name='category_list')
+    path('category/', views.category_view, name='category_list'),
+    path('new_order/', views.new_order, name='new_order'),
+    path('order_detail/<int:order_id>/', views.order_detail, name='order_detail'),
+    path('order_detail_by_table_number/<int:table_number>/', views.order_detail_by_table_number, name='order_detail_by_table_number')
 ]
